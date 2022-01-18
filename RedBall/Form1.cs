21b5a40e4d1b75996def36a40f694c6d9b24db21
grizi <grizi@GRIZIMIN-COMPUT>
2022-01-18 20:52:09 +0500

    Добавьте файлы проекта.

---
+++ RedBall/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedBall
+{
+    public partial class Form1 : Form
+    {
+        Graphics g, gf;
+        Bitmap frame;
+        GameManager gm = new GameManager();
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+            frame = new Bitmap(Width, Height);
+            gf = Graphics.FromImage(frame);
+            new RBall(gm, gf, new Point(300, 200), new Size(10, 10));        
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Step();
+            Render();
+            
+        }
+        public void Step()
+        {
+            foreach (GameObject gameObject in gm.objs) gameObject.Step();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            gm.keys.Add(e.KeyCode);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            gm.keys.Remove(e.KeyCode);        
+        }
+
+        public void Render()
+        {
+            gf.Clear(Color.LightBlue);
+            foreach (GameObject gameObject in gm.objs) gameObject.Render();
+            g.DrawImage(frame, 0, 0);
+        }
+    }
+}
---
+++ RedBall/GameObjects.cs
@@ -0,0 +1,83 @@
+﻿using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedBall
+{
+    
+    abstract class GameObject
+    {
+        public bool isGravity;
+        public float elasticity;
+        public Vector forse;
+        public Graphics g;
+        public GameManager gm;
+        public PointF pos;
+        public Size size;
+
+        protected void init(GameManager gm, Graphics g,Point p, Size size)
+        {
+            this.g = g;
+            this.gm = gm;
+            this.pos = p;
+            this.size = size;
+            forse = new Vector(0, 0);
+            gm.objs.Add(this); 
+        }
+        public void Gravity() => forse += new Vector(0, 0.09f);
+        public void AddForse(float X, float Y) => forse += new Vector(X, Y);
+        public void AddForse(Vector v) => forse += v;
+        public abstract void Render();
+        public abstract void Step();
+    }
+    class RBall : GameObject
+    {
+        public RBall(GameManager gm, Graphics g,Point p,Size size)
+        {
+            init(gm, g,p,size);
+            elasticity = 0.4;
+        }
+        public override void Render()
+        {
+            g.DrawEllipse(new Pen(Color.Red), pos.X - size.Width / 2, pos.Y - size.Height / 2, size.Width,size.Height);
+        } 
+        public override void Step()
+        {
+            pos += forse;
+            Gravity();
+            if (pos.Y + size.Height/2 >= 1000)
+            {
+                pos.Y = 1000 - size.Height / 2;
+                forse = -forse*elasticity;
+            }
+        }
+
+    }
+    class GameManager
+    {
+        public List<GameObject> objs = new List<GameObject>();
+        public List<Keys> keys = new List<Keys>();
+
+    }
+    class Vector
+    {
+        public Vector(float X, float Y)
+        {
+            x = X;
+            y = Y;
+        }
+        public float x, y;
+        public float getLength() =>(float) Math.Sqrt(x * x + y * y);
+        public static Vector operator +(Vector V1, Vector V2) => new Vector(V1.x + V2.x, V1.y + V2.y);
+        public static Vector operator -(Vector V1, Vector V2) => new Vector(V1.x - V2.x, V1.y - V2.y);
+        public static PointF operator +(PointF p,Vector v) => new PointF(v.x + p.X, v.y + p.Y);
+        public static PointF operator -(PointF p,Vector v) => new PointF(v.x - p.X, v.y - p.Y);
+        public static Vector operator *(Vector v, float f) => new Vector(v.x * f, v.y * f);
+        public static Vector operator /(Vector v, float f) => new Vector(v.x / f, v.y / f);
+        public static Vector operator -(Vector V) => new Vector(-V.x,-V.y);
+    }
+}
