138d3a91edc78fd0c4049c198b44ce2375e3670d
Григорий Зимин <grizimin@gmail.com>
2022-02-08 20:56:58 +0500

    New

--- RedBall/Form1.cs
+++ RedBall/Form1.cs
@@ -20,9 +20,16 @@ namespace RedBall
 
         private void LevelBuild()
         {
-            new RBall(gm, gf, new PointF(500, 500), new SizeF(500, 500));
-            new Platform(gm, gf, new PointF( -500, 1500), new SizeF(1000, 1));
-            new Platform(gm, gf, new PointF( -500, 500), new SizeF(1000, 1));
+            new RBall(gm, gf, new PointF(250, 250), new SizeF(500, 500));
+            new Platform(gm, gf, new PointF( -500, 1500), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF( 700, 500), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF(2000, -500), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF(3300, -1500), new SizeF(2500, 0));
+            new Platform(gm, gf, new PointF(5800, -2200), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF(4800, -2900), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF(5800, -3600), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF(4800, -4300), new SizeF(1000, 0));
+            new Platform(gm, gf, new PointF(5800, -5000), new SizeF(2500, 0));
         }
         private void timer_Tick(object sender, EventArgs e)
         {
@@ -49,7 +56,7 @@ namespace RedBall
             if (e.KeyCode == Keys.Up) gm.posCamera.Y -= 2;
             if (e.KeyCode == Keys.Left) gm.posCamera.X -= 2;
             if (e.KeyCode == Keys.Right) gm.posCamera.X += 2;
-            if ((e.KeyCode == Keys.Space || e.KeyCode == Keys.W)) gm.ball.AddForse(0, -Data.forseJump);
+            if ((e.KeyCode == Keys.Space || e.KeyCode == Keys.W) && !gm.ball.isFly) gm.ball.AddForse(0, -Data.forseJump);
             if (e.KeyCode == Keys.D) gm.ball.AddForse(Data.speedBall, 0);
             if (e.KeyCode == Keys.A) gm.ball.AddForse(-Data.speedBall, 0);
 
@@ -64,7 +71,7 @@ namespace RedBall
         {
             gf.Clear(Color.LightBlue);
             foreach (GameObject gameObject in gm.objs) gameObject.Render();
-            gf.DrawString(gm.ball.forse.ToString(), new Font("Calibri", 16), new SolidBrush(Color.Black), new Point(5, 5));
+            gf.DrawString(gm.ball.forse.ToString() + gm.ball.isFly.ToString(), new Font("Calibri", 16), new SolidBrush(Color.Black), new Point(5, 5));
             g.DrawImage(frame, 0, 0);
         }
     }
--- RedBall/GameObjects.cs
+++ RedBall/GameObjects.cs
@@ -1,14 +1,11 @@
-﻿using System.Drawing;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace RedBall
 {
-    
+
     abstract class GameObject
     {
         public bool isGravity;
@@ -18,71 +15,99 @@ namespace RedBall
         public GameManager gm;
         public PointF pos;
         public SizeF size;
-        
+        public bool isFly;
 
-        protected void init(GameManager gm, Graphics g,PointF p, SizeF size)
+
+        protected void init(GameManager gm, Graphics g, PointF p, SizeF size)
         {
             this.g = g;
             this.gm = gm;
             this.pos = p;
             this.size = size;
             forse = new Vector(0, 0);
-            gm.objs.Add(this); 
+            gm.objs.Add(this);
         }
         public PointF GetRenderPos() => new PointF(pos.X / gm.scale + 500 - gm.posCamera.X, pos.Y / gm.scale + 500 - gm.posCamera.Y);
-        public SizeF GetRenderSize() => new SizeF(size.Width / gm.scale, size.Height / gm.scale );
+        public SizeF GetRenderSize() => new SizeF(size.Width / gm.scale, size.Height / gm.scale);
         public void Gravity() => forse.y = Math.Min(forse.y + Data.gravityValue, Data.YSpeedMax);
+        public void UnGravity() => forse.y -= Data.gravityValue;
+        public float Dist(PointF p1, PointF p2) => (float)Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
         public void AddForse(float X, float Y) => forse += new Vector(X, Y);
         public void AddForse(Vector v) => forse += v;
         //public bool isfly() => !(pos.Y + size.Height / 2 >= gm.objs[1].pos.Y);
-        public abstract void Render();        
+        public abstract void Render();
         public abstract void Step();
-       
+
     }
+
     class RBall : GameObject
     {
-        public RBall(GameManager gm, Graphics g,PointF p,SizeF size)
+        public RBall(GameManager gm, Graphics g, PointF p, SizeF size)
         {
-            init(gm, g,p,size);
+            init(gm, g, p, size);
             elasticity = 0.4F;
             gm.ball = this;
         }
         public override void Render()
         {
             g.DrawEllipse(new Pen(Color.Red), GetRenderPos().X - GetRenderSize().Width / 2, GetRenderPos().Y - GetRenderSize().Height / 2, GetRenderSize().Width, GetRenderSize().Height);
-        }        
+        }
         public override void Step()
         {
             forse.y = (int)forse.y;
             forse.x = Math.Min(forse.x, Data.XSpeedMax);
             forse.x = Math.Max(forse.x, -Data.XSpeedMax);
             pos += forse;
-           //if (isfly())
-            Gravity();
+            //if (isfly())
+            isFly = true;
             foreach (var v in gm.platforms)
+            {
                 if (pos.Y + size.Height / 2 >= v.pos.Y && pos.Y + size.Height / 2 <= v.pos.Y + Data.YSpeedMax && forse.y >= 0 && pos.X >= v.pos.X && pos.X <= (v.pos + v.size).X)
                 {
+
                     pos.Y = v.pos.Y - size.Height / 2;
                     forse.y = -forse.y * elasticity;
                     forse.x = forse.x / 2;
+                    isFly = false;
                 }
+                else
+                {
+                    if (pos.Y < v.pos.Y && (Math.Abs(new Vector(pos, v.pos).getLength() - size.Height / 2) < size.Height / 25))
+                    {
+
+                        forse = new Vector(v.pos, pos).ToUnitVector() * forse.getLength();
+                        isFly = false;
+                    }
+                    if (pos.Y < v.pos.Y && (Math.Abs(new Vector(pos, v.pos + size).getLength() - size.Height / 2) < size.Height / 25))
+                    {
+
+                        forse = new Vector(v.pos + size, pos).ToUnitVector() * forse.getLength();
+                        isFly = false;
+                    }
+
+                }
+
+            }
+            if (isFly)
+                Gravity();
+
             gm.posCamera = new PointF(pos.X / gm.scale + 150, pos.Y / gm.scale);
         }
-        
+
 
     }
     class Platform : GameObject
-    {       
-        public Platform(GameManager gm, Graphics g, PointF p,SizeF size) 
+    {
+        public Platform(GameManager gm, Graphics g, PointF p, SizeF size)
         {
-            init(gm, g, p, size);  
+            init(gm, g, p, size);
             gm.platforms.Add(this);
         }
         public override void Render()
         {
-            g.DrawLine(new Pen(Color.Brown), GetRenderPos(),GetRenderPos() + GetRenderSize());
+            g.DrawLine(new Pen(Color.Brown), GetRenderPos(), GetRenderPos() + GetRenderSize());
         }
-        public override void Step() { }        
+        public override void Step() { }
     }
     class GameManager
     {
@@ -90,7 +115,7 @@ namespace RedBall
         public List<Platform> platforms = new List<Platform>();
         public List<Keys> keys = new List<Keys>();
         public RBall ball;
-        public PointF posCamera = new PointF(0,0);
+        public PointF posCamera = new PointF(0, 0);
         public int scale = 6;
     }
     class Vector
@@ -100,20 +125,23 @@ namespace RedBall
             x = X;
             y = Y;
         }
+        public Vector(PointF p1, PointF p2) => new Vector(p2.X - p1.X, p2.Y - p1.Y);
         public float x, y;
-        public float getLength() =>(float) Math.Sqrt(x * x + y * y);
+        public float getLength() => (float)Math.Sqrt(x * x + y * y);
         public static Vector operator +(Vector V1, Vector V2) => new Vector(V1.x + V2.x, V1.y + V2.y);
         public static Vector operator -(Vector V1, Vector V2) => new Vector(V1.x - V2.x, V1.y - V2.y);
-        public static PointF operator +(PointF p,Vector v) => new PointF(v.x + p.X, v.y + p.Y);
-        public static PointF operator -(PointF p,Vector v) => new PointF(v.x - p.X, v.y - p.Y);
+        public static PointF operator +(PointF p, Vector v) => new PointF(v.x + p.X, v.y + p.Y);
+        public static PointF operator -(PointF p, Vector v) => new PointF(v.x - p.X, v.y - p.Y);
         public static Vector operator *(Vector v, float f) => new Vector(v.x * f, v.y * f);
         public static Vector operator /(Vector v, float f) => new Vector(v.x / f, v.y / f);
-        public static Vector operator -(Vector V) => new Vector(-V.x,-V.y);
+        public static Vector operator -(Vector V) => new Vector(-V.x, -V.y);
         public PointF ToPointF() => new PointF(x, y);
+        public Vector ToUnitVector() => this / getLength();
+
         public override string ToString()
         {
             return $"({x};{y})";
         }
     }
-   
+
 }
